13749e7860acd55a2b3c4fa0ebde52380504cffd
maxiaobu1999 <maqinglong1999@163.com>
2019-06-08 21:27:42 +0800

    【集成】集成myBatis

---
+++ integration/src/main/java/com/norman/integration/controller/MybatisController.java
@@ -0,0 +1,25 @@
+package com.norman.integration.controller;
+
+import com.norman.integration.domain.User;
+import com.norman.integration.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+public class MybatisController {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @RequestMapping("/query")
+    @ResponseBody
+    public List<User> queryUserList(){
+        List<User> users = userMapper.queryUserList();
+        return users;
+    }
+
+}
---
+++ integration/src/main/java/com/norman/integration/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.norman.integration.mapper;
+
+import com.norman.integration.domain.User;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface UserMapper {
+
+    public List<User> queryUserList();
+
+}
