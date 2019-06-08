cb18eebdc88fe5c3218ba78a692b1ee4bf05d0c6
maxiaobu1999 <maqinglong1999@163.com>
2019-06-08 17:25:35 +0800

    【功能】如何从。yml中读取数据，两种方法     通过@Value映射数据     通过@ConfigurationProperties 映射数据

--- src/main/java/com/norman/quickstart/controller/Quick2Controller.java
+++ src/main/java/com/norman/quickstart/controller/Quick2Controller.java
@@ -5,21 +5,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+/**
+ * 通过@Value映射数据
+ * 如何从。yml中读取数据1
+ */
 @Controller
 public class Quick2Controller {
 
-//    @Value("${name}")
-//    private String name;
-//
-//    @Value("${person.addr}")
-//    private String addr;
-//
-//    @RequestMapping("/quick2")
-//    @ResponseBody
-//    public String quick2(){
-//        //获得配置文件的信息
-//
-//        return "name:"+name+",addr="+addr;
-//    }
+    @Value("${name}")
+    private String name;
+
+    @Value("${person.addr}")
+    private String addr;
+
+    @RequestMapping("/quick2")
+    @ResponseBody
+    public String quick2() {
+        //获得配置文件的信息
+
+        return "name:" + name + ",addr=" + addr;
+    }
 
 }
--- src/main/java/com/norman/quickstart/controller/Quick3Controller.java
+++ src/main/java/com/norman/quickstart/controller/Quick3Controller.java
@@ -5,42 +5,46 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+/**
+ * 通过@ConfigurationProperties 映射数据
+ * 如何从。yml中读取数据2，自动匹配
+ */
 @Controller
 @ConfigurationProperties(prefix = "person")
 public class Quick3Controller {
 
-//    private String name;
-//    private String addr;
-//    private Integer age;
-//
-//    @RequestMapping("/quick3")
-//    @ResponseBody
-//    public String quick2(){
-//        //获得配置文件的信息
-//        return "name:"+name+",addr="+addr+",age="+age;
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//
-//    public String getAddr() {
-//        return addr;
-//    }
-//
-//    public void setAddr(String addr) {
-//        this.addr = addr;
-//    }
-//
-//    public Integer getAge() {
-//        return age;
-//    }
-//
-//    public void setAge(Integer age) {
-//        this.age = age;
-//    }
+    private String name;
+    private String addr;
+    private Integer age;
+
+    @RequestMapping("/quick3")
+    @ResponseBody
+    public String quick2(){
+        //获得配置文件的信息
+        return "name:"+name+",addr="+addr+",age="+age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddr() {
+        return addr;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 }
